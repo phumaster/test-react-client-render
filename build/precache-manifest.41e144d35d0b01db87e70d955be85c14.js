871701da2b817c64125e8a344ba78fb00ce5910f
pham-ngoc-phu <pham.ngoc.phu@linecorp.com>
2019-03-11 16:33:41 +0700

    deploy

---
+++ build/precache-manifest.41e144d35d0b01db87e70d955be85c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ee19cbd57a1baefa5cca",
+    "url": "/static/js/main.946f0e7c.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "ee19cbd57a1baefa5cca",
+    "url": "/static/css/main.6ed68bca.chunk.css"
+  },
+  {
+    "revision": "f737776943cd479c0fd6c504d54994ce",
+    "url": "/index.html"
+  }
+];
